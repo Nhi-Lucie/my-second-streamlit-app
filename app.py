f5f7c69309272614965defa8ad6691bb56dc6f54
mitsuko1516 <109662357+Nhi-Lucie@users.noreply.github.com>
2023-03-17 23:55:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import sklearn
 import pickle
 
 st.title("REVENUE PREDICTION")
-# a = st.number_input('Input Temperature')
+a = st.number_input('Input Temperature')
 
-a = np.array([st.number_input('Input Temperature')])
+a = np.array(a)
 model = pickle.load(open('model.pickle', "rb"))
+x_new = a.reshape(-1, 1)
+y_new = model.predict(x_new)
 
 if st.button('Predict'):
   st.write(y_new)
